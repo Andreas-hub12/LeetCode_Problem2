cc5aa32de51f47a95b8631f4ddd51c2a494e1e6f
Andreas Kylle Camacho <andreaskylle.camacho@cit.edu>
2026-02-24 18:44:19 +0800

    Add LeetCode #2 solution and README

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// Solution class
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int x = (l1 != nullptr) ? l1->val : 0;
+            int y = (l2 != nullptr) ? l2->val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+
+        return dummy->next;
+    }
+};
+
+// Helper function to print linked list
+void printList(ListNode* node) {
+    while (node != nullptr) {
+        cout << node->val << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+// Main function
+int main() {
+    // Example input: l1 = [2,4,3], l2 = [5,6,4]
+    ListNode* l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+    ListNode* l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+    Solution sol;
+    ListNode* result = sol.addTwoNumbers(l1, l2);
+
+    cout << "Output: ";
+    printList(result);  // Expected output: 7 0 8
+
+    return 0;
+}
